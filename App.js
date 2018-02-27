7ed5f3c7f7d8c808fbdd9340e82e5b345db2bfb3
Walter Nunes Filho <jnunes.walter@gmail.com>
2018-02-27 00:04:47 -0300

    Start Tab navigation and initial activities

--- App.js
+++ App.js
@@ -1,13 +1,60 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Platform, StatusBar  } from 'react-native';
+import { TabNavigator } from 'react-navigation'
+import { purple, white } from './utils/colors'
+import { FontAwesome, Ionicons } from '@expo/vector-icons'
+import DeckList from './components/DeckList'
+import AddDeck from './components/AddDeck'
+import { Constants } from 'expo'
+
+function FlashCardStatusBar ({backgroundColor, ...props}) {
+  return (
+    <View style={{ backgroundColor, height: Constants.statusBarHeight }}>
+      <StatusBar translucent backgroundColor={backgroundColor} {...props} />
+    </View>
+  )
+ }
+
+const Tabs = TabNavigator({
+  DeckList: {
+    screen: DeckList,
+    navigationOptions: {
+      tabBarLabel: 'History'
+    },
+ },
+  AddDeck: {
+     screen: AddDeck,
+    navigationOptions: {
+      tabBarLabel: 'Add Entry'
+    },
+  },
+}, {
+  navigationOptions: {
+    header: null
+  },
+  tabBarOptions: {
+    activeTintColor: Platform.OS === 'ios' ? purple : white,
+    style: {
+      height: 56,
+      backgroundColor: Platform.OS === 'ios' ? white : purple,
+      shadowColor: 'rgba(0, 0, 0, 0.24)',
+      shadowOffset: {
+        width: 0,
+        height: 3
+      },
+      shadowRadius: 6,
+      shadowOpacity: 1
+    }
+  }
+})
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+      <View style={{flex: 1}}>
+        <FlashCardStatusBar backgroundColor={purple} barStyle="light-content" />
+        <Tabs />
+        <Text> ffffdf </Text>
       </View>
     );
   }
---
+++ components/AddDeck.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react'
+import { View, Text, StyleSheet, Platform } from 'react-native'
+import { white } from '../utils/colors'
+
+class AddDeck extends Component {
+  state = {
+    ready: false,
+  }
+  render() {
+    return (
+      <View style={styles.item}>
+        <Text >Tou didnt log any data on this day. </Text>
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  item: {
+    borderRadius: Platform.OS === 'ios' ? 16 : 2,
+    padding: 20,
+    marginLeft: 10,
+    marginRight: 10,
+    marginTop: 17,
+    justifyContent: 'center',
+    shadowRadius: 3,
+    shadowOpacity: 0.8,
+    shadowColor: 'rgba(0, 0, 0, 0.24)',
+    shadowOffset: {
+      width: 0,
+      height: 3
+    },
+  },
+  noDataText: {
+    fontSize: 20,
+    paddingTop: 20,
+    paddingBottom: 20
+  }
+})
+export default AddDeck;
